fcdac82e39b5e1dd6ce1c42b4a0208f2f1db531f
Abbas Egbeyemi <abbasegbeyemi@gmail.com>
2020-10-01 07:09:35 +0100

    Added type declarations for the svg and yaml files to tsconfig.json. Rgds.

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -3,3 +3,7 @@ declare module '*.yaml' {
   const content: [{ section: { name: string; title: string; text: string } }];
   export default content;
 }
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
